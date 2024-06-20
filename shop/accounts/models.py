8e2f146f7987cb10951a1d23f574a0d1b4e04b9f
Anatol <148008027+MagicevAnatol@users.noreply.github.com>
2024-06-20 17:04:29 +0300

    add gitignore

--- shop/accounts/models.py
+++ shop/accounts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Функция для указания пути загрузки аватара
+def avatar_image_path(instance: "Profile", filename: str) -> str:
+    return f'profiles/user_{instance.user.pk}/avatar/{filename}'
+
+
+# Основная модель пользователя
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    full_name = models.CharField(max_length=255, blank=True)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20, blank=True)
+    avatar_src = models.ImageField(upload_to=avatar_image_path, blank=True, null=True)
+    avatar_alt = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.full_name
