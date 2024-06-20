094bdf85793c953efb921ce666f50e495690cabb
Anatol <148008027+MagicevAnatol@users.noreply.github.com>
2024-06-20 17:06:52 +0300

    add frontend

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "frontend",
+    "accounts.apps.AccountsConfig",
+    "products.apps.ProductsConfig",
+    "orders.apps.OrdersConfig",
+
 ]
 
 MIDDLEWARE = [
